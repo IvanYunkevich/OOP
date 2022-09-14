884edc6f7108a1d26bd7f5977ee291e6bb413c7b
IvanYunkevich <ivan01089@mail.ru>
2022-09-14 14:14:08 +0300

    6task

---
+++ 6task/6.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+//=====Task1==========================================================================
+void writeInt()
+{
+	int num;
+
+	cout << "Enter an int : ";
+
+	while (true)
+	{
+		cin >> num;
+
+		if (cin.good() && cin.peek() == '\n')
+		{
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			break;
+		}
+		cin.clear();
+		cout << "You entered an invalid number, try again : ";
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	}
+	cout << "You entered an integer : " << num << endl;
+}
+//=====Task2==========================================================================
+ostream& endll(ostream& s)
+{
+	s << "\n\n";
+	s.flush();
+	return s;
+}
+int main()
+{
+	writeInt();
+	cout << "test" << endll;
+	cout << "test";
+	return 0;
+}
---
+++ 6task/BlackJack.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+using namespace std;
+//=======Task3======================================================================
+class Player : public GenericPlayer
+{
+public:
+    Player(const string& name = "") : GenericPlayer(name) { }
+
+    virtual ~Player() { }
+
+    virtual bool IsHitting() const
+    {
+        cout << _Name << ", do you want a hit? (Y/N): ";
+        char response;
+        cin >> response;
+        return (response == 'y' || response == 'Y');
+    }
+
+    void Win() const
+    {
+        cout << _Name << " wins.\n";
+    }
+
+    void Lose() const
+    {
+        cout << _Name << " loses.\n";
+    }
+
+    void Push() const
+    {
+        cout << _Name << " pushes.\n";
+    }
+};
+//=====task4==========================================================================
+class House : public GenericPlayer
+{
+public:
+    House(const string& name = "House") : GenericPlayer(name) { }
+
+    virtual ~House() { }
+
+    virtual bool IsHitting() const
+    {
+        return (GetTotal() <= 16);
+    }
+
+    void FlipFirstCard()
+    {
+        if (!(_Cards.empty()))
+            _Cards[0]->Flip();
+        else
+            cout << "No card to flip!\n";
+    }
+};
+int main()
+{
+    std::cout << "Hello World!\n";
+}
